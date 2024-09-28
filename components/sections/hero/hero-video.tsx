7e2dea696fe020d3861e90fa9a7d406bd09d8ba1
hasanharman <hasanharman33@gmail.com>
2024-09-28 15:11:06 +0300

    Hero video added

--- components/sections/hero/hero-video.tsx
+++ components/sections/hero/hero-video.tsx
@@ -2,13 +2,12 @@
 
 import { useState } from "react";
 import Image from "next/image";
-// import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 
-// import { Play, XIcon } from "lucide-react";
+import { Play, XIcon } from "lucide-react";
 import ThumbnailImage from "@/assets/demo.png";
-
 
 type AnimationStyle =
   | "from-bottom"
@@ -73,8 +72,8 @@ const animationVariants = {
 
 export default function HeroVideoDialog({
   animationStyle = "from-center",
-  // videoSrc,
-  // thumbnailSrc,
+  videoSrc,
+  thumbnailSrc,
   thumbnailAlt = "Video thumbnail",
   className,
 }: HeroVideoProps) {
@@ -94,7 +93,7 @@ export default function HeroVideoDialog({
           height={1080}
           className="transition-all duration-200 group-hover:brightness-[0.8] ease-out rounded-md border"
         />
-        {/* <div className="absolute inset-0 flex items-center justify-center group-hover:scale-100 scale-[0.9] transition-all duration-200 ease-out rounded-2xl">
+        <div className="absolute inset-0 flex items-center justify-center group-hover:scale-100 scale-[0.9] transition-all duration-200 ease-out rounded-2xl">
           <div className="z-30 bg-primary/10 flex items-center justify-center rounded-full backdrop-blur-md size-28">
             <div
               className={`flex items-center justify-center bg-gradient-to-b from-primary/30 to-primary shadow-md rounded-full size-20 transition-all ease-out duration-200 relative group-hover:scale-[1.2] scale-100`}
@@ -108,9 +107,9 @@ export default function HeroVideoDialog({
               />
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
-      {/* <AnimatePresence>
+      <AnimatePresence>
         {isVideoOpen && (
           <motion.div
             initial={{ opacity: 0 }}
@@ -138,7 +137,7 @@ export default function HeroVideoDialog({
             </motion.div>
           </motion.div>
         )}
-      </AnimatePresence> */}
+      </AnimatePresence>
     </div>
   );
 }
--- components/sections/hero/index.tsx
+++ components/sections/hero/index.tsx
@@ -154,7 +154,7 @@ function HeroImage() {
     >
       <HeroVideoDialog
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        videoSrc="https://www.youtube.com/embed/Es3-0afAvj0"
         thumbnailSrc="/demo.png"
         thumbnailAlt="Thumbnail"
         className="border rounded-lg shadow-lg max-w-screen-lg mt-16"
